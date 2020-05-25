46db42caa793dfd4cc72726b636e298b293450af
iagox86 <ron-git@skullsecurity.org>
2020-05-25 16:09:48 -0700

    Add commit hooks for a few small tasks

---
+++ hooks/hook.d
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# This script will be copied into the .git/hooks directory, replacing the hook
+# files such as pre-commit and pre-push. Once in place, this will execute each
+# file in the various hooks/<name>.d/ directories.
+script_dir=$(git rev-parse --show-toplevel)/hooks
+hook_name=$(basename $0)
+
+if [ "$hook_name" = "hook.d" ]; then
+  echo "Don't run this script directly! Symlink it to .git/hooks/<name> instead!"
+  exit 1
+fi
+
+hook_dir="$script_dir/$hook_name.d"
+if [[ -d $hook_dir ]]; then
+  for hook in $hook_dir/*; do
+    if [ -x $hook ]; then
+      echo "Running $hook"
+      $hook "$@"
+
+      exit_code=$?
+
+      if [ $exit_code != 0 ]; then
+        exit $exit_code
+      fi
+    else
+      echo "Not executable: $hook"
+    fi
+  done
+fi
+
+exit 0
---
+++ hooks/pre-commit.d/update-readme-md.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Print errors in red
+err() {
+  echo -ne '\e[31m\e[1m' # Red + Bold
+  echo -e "$@"
+  echo -ne '\e[0m'
+  exit 0
+}
+
+# Get the root directory
+BASE=$(git rev-parse --show-toplevel)
+
+# Update README.md
+echo "Updating $BASE/README.md"
+cargo readme > $BASE/README.md || err 'Failed to run `cargo readme`!'
